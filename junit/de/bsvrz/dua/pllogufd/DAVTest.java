05572f0d0fd48f51e0f2b0d4546a88ff030a7292
tfelder <info@bitctrl.de>
2007-07-10 10:01:16 +0000

    Initial import.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.plloglve@2179 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ junit/de/bsvrz/dua/pllogufd/DAVTest.java
@@ -0,0 +1,93 @@
+package de.bsvrz.dua.pllogufd;
+
+import java.util.Random;
+
+import stauma.dav.clientside.ClientDavInterface;
+import sys.funclib.ArgumentList;
+import sys.funclib.application.StandardApplication;
+import sys.funclib.application.StandardApplicationRunner;
+
+/**
+ * Stellt eine Datenverteiler-Verbindung
+ * zur Verfügung.
+ * 
+ * @author BitCtrl Systems GmbH, Thierfelder
+ * 
+ */
+public class DAVTest {
+
+	/**
+	 * Verbindungsdaten
+	 */
+	private static final String[] CON_DATA = new String[] {
+			"-datenverteiler=192.168.1.191:8083", //$NON-NLS-1$ 
+			"-benutzer=Tester", //$NON-NLS-1$
+			"-authentifizierung=c:\\passwd1", //$NON-NLS-1$
+			"-debugLevelStdErrText=INFO", //$NON-NLS-1$
+			"-debugLevelFileText=INFO" }; //$NON-NLS-1$
+
+	/**
+	 * Verbindung zum Datenverteiler
+	 */
+	protected static ClientDavInterface VERBINDUNG = null;
+
+	/**
+	 * Randomizer
+	 */
+	public static Random R = new Random(System.currentTimeMillis());
+
+	
+	/**
+	 * Erfragt bzw. initialisiert eine
+	 * Datenverteiler-Verbindung
+	 * 
+	 * @return die Datenverteiler-Verbindung
+	 * @throws Exception falls die Verbindung nicht
+	 * hergestellt werden konnte
+	 */
+	public static final ClientDavInterface getDav()
+	throws Exception {
+		
+		if(VERBINDUNG == null) {
+			StandardApplicationRunner.run(new StandardApplication() {
+	
+				public void initialize(ClientDavInterface connection)
+						throws Exception {
+					DAVTest.VERBINDUNG = connection;
+				}
+	
+				public void parseArguments(ArgumentList argumentList)
+						throws Exception {
+					//
+				}
+	
+			}, CON_DATA);
+		}
+		
+		return VERBINDUNG;
+	}
+
+
+	/**
+	 * Erfragt einen Array mit zufälligen Zahlen von
+	 * 0 bis <code>anzahl</code>. Jede Zahl darf nur 
+	 * einmal im Array vorkommen.
+	 * 
+	 * @param anzahl die Obergrenze
+	 * @return Array mit zufälligen Zahlen von
+	 * 0 bis <code>anzahl</code>
+	 */
+	public static final int[] getZufaelligeZahlen(int anzahl){
+		int belegt = 0;
+		int[] zahlen = new int[anzahl];
+		for(int i = 0; i<anzahl; i++)zahlen[i] = -1;
+		
+		while(belegt < anzahl){
+			int index = R.nextInt(anzahl);
+			if(zahlen[index] == -1)zahlen[index] = belegt++;
+		}
+		
+		return zahlen;
+	}
+
+}
