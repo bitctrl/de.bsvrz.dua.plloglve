7ca0d7b59aaccd2b6bdd9cb75bfdd88576e100a4
goerlitz <info@bitctrl.de>
2007-08-02 05:23:03 +0000

    Ausfallhäufigkeits- und Vertrauensbereichtest separiert
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.plloglve@2714 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ junit/de/bsvrz/dua/plloglve/test/PlPruefungLogischLVETestAusfall.java
@@ -0,0 +1,70 @@
+package de.bsvrz.dua.plloglve.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.bsvrz.dua.plloglve.util.PlPruefungAusfall;
+import de.bsvrz.sys.funclib.bitctrl.dua.test.DAVTest;
+
+import stauma.dav.clientside.ClientDavInterface;
+import sys.funclib.ArgumentList;
+
+/**
+ * Automatisierter Test nach Prüfspezifikation für SWE Pl-Prüfung logisch LVE
+ * 
+ * @author Thierfelder
+ *
+ */
+public class PlPruefungLogischLVETestAusfall {
+	
+//	/**
+//	 * Verbindungsdaten
+//	 */
+//	public static final String[] CON_DATA = new String[] {
+//			"-datenverteiler=192.168.1.191:8083", //$NON-NLS-1$ 
+//			"-benutzer=Tester", //$NON-NLS-1$
+//			"-authentifizierung=c:\\passwd1" }; //$NON-NLS-1$
+
+	/**
+	 * Verbindungsdaten
+	 */
+	public static final String[] CON_DATA = new String[] {
+			"-datenverteiler=localhost:8083", //$NON-NLS-1$ 
+			"-benutzer=Tester", //$NON-NLS-1$
+			"-authentifizierung=c:\\passwd" }; //$NON-NLS-1$
+	
+	/**
+	 * Verzeichnis, in dem sich die CSV-Dateien mit den Testdaten befinden
+	 */
+	protected static final String TEST_DATEN_VERZ = ".\\testDaten\\"; //$NON-NLS-1$
+
+	/**
+	 * Datenverteiler-Verbindung
+	 */
+	private ClientDavInterface dav = null;
+	
+	/**
+	 * Loggerargument
+	 * 
+	 * Pfadangabe mit Argument: -debugFilePath=[Pfad]
+	 */
+	private String[] argumente = new String[] {"-debugLevelFileText=ALL"};
+	private ArgumentList alLogger = new ArgumentList(argumente);
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.dav = DAVTest.getDav(CON_DATA);	
+	}
+
+	/**
+	 * Ausfallhäufigkeitstest nach Prüfspezifikation
+	 */
+	@Test
+	public void testAusfall()throws Exception{
+		PlPruefungAusfall pruefAusfall = new PlPruefungAusfall(dav,TEST_DATEN_VERZ, alLogger);
+		pruefAusfall.pruefe();
+	}
+}
---
+++ junit/de/bsvrz/dua/plloglve/test/PlPruefungLogischLVETestVertrauensbereich.java
@@ -0,0 +1,70 @@
+package de.bsvrz.dua.plloglve.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.bsvrz.dua.plloglve.util.PlPruefungVertrauensbereich;
+import de.bsvrz.sys.funclib.bitctrl.dua.test.DAVTest;
+
+import stauma.dav.clientside.ClientDavInterface;
+import sys.funclib.ArgumentList;
+
+/**
+ * Automatisierter Test nach Prüfspezifikation für SWE Pl-Prüfung logisch LVE
+ * 
+ * @author Thierfelder
+ *
+ */
+public class PlPruefungLogischLVETestVertrauensbereich {
+	
+//	/**
+//	 * Verbindungsdaten
+//	 */
+//	public static final String[] CON_DATA = new String[] {
+//			"-datenverteiler=192.168.1.191:8083", //$NON-NLS-1$ 
+//			"-benutzer=Tester", //$NON-NLS-1$
+//			"-authentifizierung=c:\\passwd1" }; //$NON-NLS-1$
+
+	/**
+	 * Verbindungsdaten
+	 */
+	public static final String[] CON_DATA = new String[] {
+			"-datenverteiler=localhost:8083", //$NON-NLS-1$ 
+			"-benutzer=Tester", //$NON-NLS-1$
+			"-authentifizierung=c:\\passwd" }; //$NON-NLS-1$
+	
+	/**
+	 * Verzeichnis, in dem sich die CSV-Dateien mit den Testdaten befinden
+	 */
+	protected static final String TEST_DATEN_VERZ = ".\\testDaten\\"; //$NON-NLS-1$
+
+	/**
+	 * Datenverteiler-Verbindung
+	 */
+	private ClientDavInterface dav = null;
+	
+	/**
+	 * Loggerargument
+	 * 
+	 * Pfadangabe mit Argument: -debugFilePath=[Pfad]
+	 */
+	private String[] argumente = new String[] {"-debugLevelFileText=ALL"};
+	private ArgumentList alLogger = new ArgumentList(argumente);
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.dav = DAVTest.getDav(CON_DATA);	
+	}
+
+	/**
+	 * Vertrauensbereichstest nach Prüfspezifikation
+	 */
+	@Test
+	public void testVB()throws Exception{
+		PlPruefungVertrauensbereich pruefVertrB = new PlPruefungVertrauensbereich(dav,TEST_DATEN_VERZ, alLogger);
+		pruefVertrB.pruefe();
+	}
+}
