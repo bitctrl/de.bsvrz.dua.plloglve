69153fccfbf77dca31277dbb10b560ae0ebe5130
tfelder <info@bitctrl.de>
2008-03-25 10:43:10 +0000

    Optimiert
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.plloglve@7676 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ src/de/bsvrz/dua/plloglve/plloglve/vb/VertrauensPuffer.java
@@ -0,0 +1,58 @@
+/** 
+ * Segment 4 Datenübernahme und Aufbereitung (DUA), SWE 4.2 Pl-Prüfung logisch LVE
+ * Copyright (C) 2007 BitCtrl Systems GmbH 
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Contact Information:<br>
+ * BitCtrl Systems GmbH<br>
+ * Weißenfelser Straße 67<br>
+ * 04229 Leipzig<br>
+ * Phone: +49 341-490670<br>
+ * mailto: info@bitctrl.de
+ */
+package de.bsvrz.dua.plloglve.plloglve.vb;
+
+import de.bsvrz.sys.funclib.bitctrl.dua.intpuf.IntervallPuffer;
+
+/**
+ * Ein Puffer für Ausfalldaten innerhalb der Vertrauensbereichspruefung
+ *
+ * @author BitCtrl Systems GmbH, Thierfelder
+ *
+ */
+public class VertrauensPuffer
+extends IntervallPuffer<VertrauensDatum>{
+	
+	/**
+	 * Berechnet den Ausfall in Millisekunden
+	 * 
+	 * @return den Ausfall in Millisekunden
+	 */
+	public final long getAusfall(){
+		long ausfall = 0;
+		
+		synchronized (this.puffer) {
+			for(Intervall<VertrauensDatum> intervall:this.puffer.values()){
+				if(intervall.getInhalt().isAusgefallen()){
+					ausfall += intervall.getIntervallEnde() - intervall.getIntervallStart();
+				}
+			}			
+		}
+		
+		return ausfall;
+	}
+
+}
