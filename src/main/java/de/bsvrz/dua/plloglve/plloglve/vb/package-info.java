65824fe469593d492a5dd68b72ec041eea5d7b77
peuker <uwe.peuker@bitctrl.de>
2015-03-18 09:36:42 +0000

    Header korrigiert SVN-Keywords gesetzt
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.plloglve@53825 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/main/java/de/bsvrz/dua/plloglve/plloglve/vb/package-info.java
+++ src/main/java/de/bsvrz/dua/plloglve/plloglve/vb/package-info.java
@@ -1,7 +1,7 @@
-/** 
+/*
  * Segment 4 Datenübernahme und Aufbereitung (DUA), SWE 4.2 Pl-Prüfung logisch LVE
- * Copyright (C) 2007 BitCtrl Systems GmbH 
- * 
+ * Copyright (C) 2007-2015 BitCtrl Systems GmbH
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
@@ -23,6 +23,8 @@
  * Phone: +49 341-490670<br>
  * mailto: info@bitctrl.de
  */
+
+
 
 /**
  * Alle Klassen des Submoduls Vertrauensbereich
